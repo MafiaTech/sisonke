52f52195dd5d3466c6bdbd646e16c3c5d49c9f6f
GM Tech <gontsempshe@gmail.com>
2026-06-02 16:13:29 +0200

    Checkpoint member portal functionality and UI refinements

--- Sisonke.Web/obj/Debug/net10.0/Sisonke.Web.AssemblyInfo.cs
+++ Sisonke.Web/obj/Debug/net10.0/Sisonke.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sisonke.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86286b3ec7d9e5e8729cfd070335c8b1e93dc2d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cf7cf3b5d21bf6eee853a40054455a7fc0422f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sisonke.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sisonke.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
